06c78f243628dada3d4775c8311b1c46d52e740d
AtomicConglomerate <mikael.marois@gmail.com>
2016-03-23 11:23:41 -0400

    Fixed typo

--- demon_stack.m
+++ demon_stack.m
@@ -8,4 +8,3 @@ demon_stack=zeros(size(stack));
 parfor i=1:size(stack,3);
     [~,demon_stack(:,:,i)]=imregdemons(stack(:,:,i),stack(:,:,1),N);
 end
-[~,demon_stack(:,:,i)]=imregdemons(stack(:,:,i),stack(:,:,1),N);    
