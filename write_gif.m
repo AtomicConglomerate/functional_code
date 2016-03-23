66c2927bad34cc603192bd2c7d48474f9fbb66e5
Mikael Marois <mikael.marois@gmail.com>
2016-03-23 16:04:27 -0400

    Derped some stuff

--- write_gif.m
+++ write_gif.m
@@ -3,13 +3,13 @@ function write_gif(im_stack, start, nframes, filename)
 % Creates a nframes gif starting from a specific frame
 
 if start+nframes > size(im_stack,3)
-    fprint('Start + nframes greater than length of recording, gif will be shortened');
+    fprintf('Start + nframes greater than length of recording, gif will be shortened\n');
     nframes=size(im_stack,3)-start;
 end
 
 imwrite(255*im_stack(:,:,start),filename,'gif', 'Loopcount',inf);
 
 for i=1:nframes
-    imwrite(255*im_stack(:,:,start+nframes),filename,'gif','WriteMode','append');
+    imwrite(255*im_stack(:,:,start+i),filename,'gif','WriteMode','append','DelayTime',0);
 end
 
